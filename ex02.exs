a91f92eb9c8797f9795de28d44e2c551deb0cc83
trihazard <nsrirama@smu.edu>
2019-10-10 21:13:14 -0500

    got ex02 to work

--- ex02.exs
+++ ex02.exs
@@ -31,10 +31,10 @@ defmodule Test do
   """
 
   test "counter using an agent" do
-    { :ok, counter } = «your code goes here»
-    value   = Agent.get_and_update(counter, «your code here»)
+    { :ok, counter } = Agent.start_link(fn -> 0 end)
+    value   = Agent.get_and_update(counter, fn value -> {value, value+1} end)
     assert value == 0
-    value   = Agent.get_and_update(counter, «your code here»)
+    value   = Agent.get_and_update(counter, fn value -> {value, value+1} end)
     assert value == 1
   end
 
@@ -46,7 +46,7 @@ defmodule Test do
   5 points
   """
 
-  @tag :skip
+  #@tag :skip
 
   test "higher level API interface" do
     count = Ex02.new_counter(5)
@@ -63,7 +63,7 @@ defmodule Test do
   5 points
   """
 
-  @tag :skip
+  #@tag :skip
   test "global counter" do
     Ex02.new_global_counter
     assert Ex02.global_next_value == 0
@@ -82,10 +82,13 @@ defmodule Ex02 do
 
   def new_counter(initial_value \\ 0) do
     # « your code goes here»
+    {:ok, agent} = Agent.start_link(fn -> initial_value end)
+    agent
   end
 
   def next_value(agent) do
     # « your code goes here»
+    Agent.get_and_update(agent, fn value -> {value, value+1} end)
   end
 
   @global_name :my_global_agent
@@ -94,11 +97,13 @@ defmodule Ex02 do
     # « your code goes here»
     # You can give an agent a name by adding the
     # `name: @global_name` option to start_link
+    Agent.start_link(fn-> initial_value end, name: @global_name)
   end
 
   def global_next_value do
     # « your code goes here»
     # and you can use that name instead of a pid
     # in calls such as `get_and_update`
+    Agent.get_and_update(:my_global_agent, fn value -> {value, value+1} end)
   end
 end
