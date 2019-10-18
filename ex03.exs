878ac1febb20ed895ac7d35f38aa91b845db5d05
jackrosenblatt <jdrosenblatt18@gmail.com>
2019-10-18 18:08:10 -0500

    ex03 runs tests with 0 failures

--- ex03.exs
+++ ex03.exs
@@ -27,7 +27,7 @@ defmodule Ex03 do
 
   Your mission is to implement a function
 
-      pmap(collection, process_count, func)
+	  pmap(collection, process_count, func)
 
   This will take the collection, split it into n chunks, where n is
   the process count, and then run each chunk through a regular map
@@ -45,17 +45,37 @@ defmodule Ex03 do
   six helper functions.
 
   35 points:
-     it works and passes all tests:    25
-     it contains no conditional logic:  3
-     it is nicely structured            7
+	 it works and passes all tests:    25
+	 it contains no conditional logic:  3
+	 it is nicely structured            7
   """
 
-  def pmap(collection, process_count, function) do
-    # your code goes here
-    # I'm hoping to see a simple pipeline as the body of this function...
-  end
-
-  # and here...
+	def pmap(collection, process_count, function) do
+		splitCollection(collection, process_count)
+		|> mapCollections(function)
+	end
+	
+	#Divides the collection into n groups where n is process_count
+	def splitCollection(collection, process_count) do
+		collection |> Enum.chunk_every(trunc(Enum.count(collection) / process_count))
+	end
+	
+	#Maps each sub collection to a process
+	def mapCollections(collections, function) do
+		Enum.map(collections, fn collection -> spawnProcess(collection, function) end)
+		|> Enum.flat_map(fn pid -> (receive do {^pid, val} -> val end) end)
+	end
+	
+	#Spawns a process on the sub collection
+	def spawnProcess(collection, function) do
+		spawn Ex03, :runFunction, [collection, function, self()]
+	end
+	
+	#Runs the function on the sub collection and returns the sub collection after the function run
+	def runFunction(collection, function, from) do
+		newCollection = collection |> Enum.map(fn value -> function.(value) end)
+		send from, {self(), newCollection}
+	end
 
 end
 
@@ -71,40 +91,40 @@ defmodule TestEx03 do
   @expected 2..11 |> Enum.into([])
 
   test "pmap with 1 process" do
-    assert pmap(1..10, 1, &(&1+1)) == @expected
+	assert pmap(1..10, 1, &(&1+1)) == @expected
   end
 
   test "pmap with 2 processes" do
-    assert pmap(1..10, 2, &(&1+1)) == @expected
+	assert pmap(1..10, 2, &(&1+1)) == @expected
   end
 
   test "pmap with 3 processes (doesn't evenly divide data)" do
-    assert pmap(1..10, 3, &(&1+1)) == @expected
+	assert pmap(1..10, 3, &(&1+1)) == @expected
   end
 
   test "actually reduces time" do
-    range = 1..6
+	range = 1..6
 
-    # random calculation to burn some time.
-    # Note that the sleep value reduces
-    # with successive values, so the
-    # later values will complete firest. Does
-    # your code correctl;y gather the results in the
-    # right order?
+	# random calculation to burn some time.
+	# Note that the sleep value reduces
+	# with successive values, so the
+	# later values will complete firest. Does
+	# your code correctl;y gather the results in the
+	# right order?
 
-    calc  = fn n -> :timer.sleep(10-n); n*3 end
+	calc  = fn n -> :timer.sleep(10-n); n*3 end
 
-    { time1, result1 } = :timer.tc(fn -> pmap(range, 1, calc) end)
-    { time2, result2 } = :timer.tc(fn -> pmap(range, 2, calc) end)
-    { time3, result3 } = :timer.tc(fn -> pmap(range, 3, calc) end)
+	{ time1, result1 } = :timer.tc(fn -> pmap(range, 1, calc) end)
+	{ time2, result2 } = :timer.tc(fn -> pmap(range, 2, calc) end)
+	{ time3, result3 } = :timer.tc(fn -> pmap(range, 3, calc) end)
 
-    expected = 1..6 |> Enum.map(&(&1*3))
-    assert result1 == expected
-    assert result2 == expected
-    assert result3 == expected
+	expected = 1..6 |> Enum.map(&(&1*3))
+	assert result1 == expected
+	assert result2 == expected
+	assert result3 == expected
 
-    assert time2 < time1 * 0.75   # in theory should be 0.5
-    assert time3 < time1 * 0.45   # and 0.33
+	assert time2 < time1 * 0.75   # in theory should be 0.5
+	assert time3 < time1 * 0.45   # and 0.33
   end
 
 end
