30d45bb700551987a989702923801760515725b7
mariomanoptic <47004969+mariomanoptic@users.noreply.github.com>
2019-10-18 23:30:18 -0500

    number 1

--- ex01.exs
+++ ex01.exs
@@ -50,7 +50,7 @@ defmodule Test do
   #
   # To test your functions, you need to delete the following line
 
-  @tag :skip
+  #@tag :skip
 
   # then rerun `elixir ex01.exs`
   #
@@ -74,14 +74,24 @@ end
 defmodule Ex01 do
 
   def counter(value \\ 0) do
-    # ...your code
+    receive do
+	{ :next, from } ->
+	  send from, {:next_is, value}
+	  counter(value+1)
+	{ :next_is, value} ->
+	   counter(value)
+    end    
   end
 
   def new_counter(initial_value \\ 0) do
-    # ... your code
+	_counting = spawn fn -> counter(initial_value) end
   end
 
   def next_value(counter_pid) do
-    # ... your code
+	send counter_pid, {:next, self()}
+	receive do
+		{:next_is,val} ->
+			val
+	end
   end
 end
